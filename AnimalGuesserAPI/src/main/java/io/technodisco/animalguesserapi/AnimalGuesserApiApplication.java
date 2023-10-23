ca6506a4bf70139c3fea805f2c55d66a043d73c4
GoldilocksZone <jmarulli@gmail.com>
2023-10-23 14:19:33 -0400

    Initialized Spring Boot Application

---
+++ AnimalGuesserAPI/src/main/java/io/technodisco/animalguesserapi/AnimalGuesserApiApplication.java
@@ -0,0 +1,13 @@
+package io.technodisco.animalguesserapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalGuesserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalGuesserApiApplication.class, args);
+	}
+
+}
---
+++ AnimalGuesserAPI/src/test/java/io/technodisco/animalguesserapi/AnimalGuesserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.technodisco.animalguesserapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalGuesserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
